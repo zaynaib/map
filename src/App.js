ba7fd2b4b69d46d337fe0f9c68223c4f2365b49e
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-27 13:45:08 -0500

    feature: foursquare api started working using javacript fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 //https://reactjs.org/docs/faq-ajax.html
 //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 //https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+// https://medium.com/@thejasonfile/fetch-vs-axios-js-for-making-http-requests-2b261cdd3af5 ***
 
 import React, { Component } from 'react'
 import './App.css'
@@ -16,28 +17,23 @@ class App extends Component {
 
   componentDidMount(){
     this.getFourSquareVenues()
-    //this.renderMap()
+    this.renderMap()
   }
 
   //create API with foursquare
   getFourSquareVenues = () =>{
     //fetch the function
-    /*
-    fetch('https://api.foursquare.com/v2/venues/explore?client_id=X1MSP3S2NAEMOOXXBN1JGB2SXSRTHMFALEEQGNJFGOS4JR1K&client_secret=H5F0ZKW53GMOT0EEFDKAS0X3NM30UXS4AQDSVVE2GHU5OGAI&near=Chicago&query=musuem&limit=15')
-          .then( (response) =>{console.log(response)})
-          .catch((err) =>{console.log(err)})
-      console.log('working')
-      */
-     fetch('https://api.foursquare.com/v2/venues/explore?client_id=X1MSP3S2NAEMOOXXBN1JGB2SXSRTHMFALEEQGNJFGOS4JR1K&client_secret=H5F0ZKW53GMOT0EEFDKAS0X3NM30UXS4AQDSVVE2GHU5OGAI&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee')
-    .then((response) => {
+
+     fetch('https://api.foursquare.com/v2/venues/explore?client_id=X1MSP3S2NAEMOOXXBN1JGB2SXSRTHMFALEEQGNJFGOS4JR1K&client_secret=H5F0ZKW53GMOT0EEFDKAS0X3NM30UXS4AQDSVVE2GHU5OGAI&&v=20180323&near=Chicago&query=musuem&limit=15')
+    .then((response) => response.json())
+     .then(data =>{
         // Code for handling API response
-        console.log(response)
+        console.log(data)
         console.log("working")
-    })
-    .catch((err) => {
-        // Code for handling errors
-        console.log('Looks like there was a problem: \n',err)
-    });
+  
+     })
+     .catch((err) =>{console.log('There is a problem',err)})
+   
   }
 
   renderMap = () => {
@@ -51,7 +47,7 @@ class App extends Component {
 
     // Create A Map
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.7243, lng: -74.0018},
+      center: {lat: 41.8781, lng: -87.6298},
       zoom: 8
     })  
     //then us map to create markers from venue
